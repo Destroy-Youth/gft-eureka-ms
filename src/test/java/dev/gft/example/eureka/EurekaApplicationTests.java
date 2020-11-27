6b70fa96338abaffdb70757e6b1ef275817dc9f4
Equihua <luis.herrerae@outlook.com>
2020-11-26 18:34:28 -0600

    eureka basic config

---
+++ src/test/java/dev/gft/example/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gft.example.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
